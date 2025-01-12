4c962fbe5947cf5e0e279dd97585961c0a28a756
petra221 <petra@student.agh.edu.pl>
2025-01-12 10:54:18 +0100

    przed zapytaniem o conorary operator

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ function App() {
 // let title = 'Martix'
 const [title, setTitle] =useState('Wall-E')
 
+let naglowek = '';
+if(title.length < 5) {
+  naglowek = "Text is too short"
+} else if(title.length < 15 ) {
+  naglowek = "That is a good title"
+} else if (title.length > 45){ 
+  naglowek = "This is too long"
+  
+}
+
+
   function handleChange(event){
     // title = event.target.value  <-- wronga aproach
     setTitle(event.target.value)
   }
 
 
-
+// cernary operator ? :
   return (
     <div className="App">
       <header className="App-header">
@@ -25,7 +36,13 @@ const [title, setTitle] =useState('Wall-E')
         <div>
         <h1>My favourite movies to watch</h1>
         <h2>My favourite movies for today to watch is {title}</h2>
-        <input type="text" onChange={handleChange}/>
+
+        {/* <div>{naglowek}</div> */}
+        
+        {title.length > 0 && <div>{naglowek}</div>}
+
+        <input type="text" value={title} onChange={handleChange}/>
+
         </div>
       </header>
     </div>  
